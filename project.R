4e59f8f35738ef720a8ad67cf1d35929a47b7525
xXtraStupidx22 <rcl232@cornell.edu>
2017-05-08 12:29:56 -0400

    project.R information gain added

--- project.R
+++ project.R
@@ -1,4 +1,19 @@
 library(data.table)
+library(FSelector)
+library(plyr)
 
 zip <- unzip("en.openfoodfacts.org.products.tsv.zip")
-temp <- fread(zip,sep='\t')
+trainSet <- fread(zip,sep='\t')
+
+trainSet <- rename(trainSet,c("nutrition-score-uk_100g"="score"))
+trainSet$url <- NULL
+trainSet$last_modified_datetime <- NULL
+trainSet$image_url <- NULL
+trainSet$image_small_url <- NULL
+trainSet$created_datetime <- NULL
+
+sampleTrain <- trainSet[sample(900, 600, replace=FALSE),]
+sampleTrain[is.na(sampleTrain)] <- 0
+
+weights <- information.gain(score~.,sampleTrain)
+
