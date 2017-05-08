b274c395db6107da7e84309326e4af121dc20c3c
xXtraStupidx22 <rcl232@cornell.edu>
2017-05-08 17:48:12 -0400

    project.R information gain added

--- project.R
+++ project.R
@@ -11,8 +11,19 @@ trainSet$last_modified_datetime <- NULL
 trainSet$image_url <- NULL
 trainSet$image_small_url <- NULL
 trainSet$created_datetime <- NULL
+trainSet$`nutrition-score-fr_100g` <- NULL
+trainSet$states <- NULL
+trainSet$states_tags <- NULL
+trainSet$states_en <- NULL
+trainSet$nutrition_grade_fr <- NULL
+trainSet$nutrition_grade_uk <- NULL
+trainSet$cities <- NULL
+trainSet$cities_tags <- NULL
+trainSet$countries <- NULL
+trainSet$countries_en <- NULL
+trainSet$countries_tags <- NULL
 
-sampleTrain <- trainSet[sample(900, 600, replace=FALSE),]
+sampleTrain <- trainSet[sample(30000, 20000, replace=FALSE),]
 sampleTrain[is.na(sampleTrain)] <- 0
 
 weights <- information.gain(score~.,sampleTrain)
